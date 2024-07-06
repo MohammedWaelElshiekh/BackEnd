27a2c283deb14fb229975283486c912af0d8b849
Mohammed Wael Elshiekh <mohammedwaelelshiekhmw@gmail.com>
2024-07-06 18:53:37 +0300

    added the flexibility of the wanted placeholder using url parameters

--- src/routes/placeholderGenerator/index.ts
+++ src/routes/placeholderGenerator/index.ts
@@ -1,13 +1,20 @@
 import sharp from "sharp";
+import { promises as fsPromises } from "fs";
 export default async function generatePlaceholder(req: any, res: any) {
-  await sharp({
-    text: {
-      width: 300,
-      // background: "gray",
-      height: 200,
-      // channels: 3,
-      text: "Hello",
-    },
-  }).toFile("noise.png");
-  res.send("worked");
+  const color = await req.query.c,
+    height = await +req.query.h,
+    width = await +req.query.w;
+  const imgName = `./cachedImagesOnServer/${color}-${await height}-${await width}.png`;
+  fsPromises.appendFile(imgName, "").then(async (data) => {
+    await sharp({
+      create: {
+        background: color ? color : "gray",
+        width: width ? width : 100,
+        height: height ? height : 100,
+        channels: 3,
+      },
+    }).toFile(imgName);
+
+    res.send("done");
+  });
 }
